ed39cd2affaaaffefab778f8da928807943a2263
patrickclery <patrick.clery@gmail.com>
2020-01-13 04:43:58 -0800

    :card_file_box: Performing database related changes. :white_check_mark: Adding tests.

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates_numericality_of :balance
+
+  validates_presence_of :account_number
+  validates_presence_of :balance
+  validates_presence_of :name
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,16 @@
 RSpec.describe User, type: :model do
 
-  # Collections
-  it { should have_db_column(:account_number).of_type(:string)  } # Allow alphanumeric
-  it { should have_db_column(:balance)       .of_type(:integer) }
-  it { should have_db_column(:name)          .of_type(:string)  }
+  it { should have_db_column(:account_number).of_type(:string).with_options(null: false) } # Allow alphanumeric
+  it { should have_db_column(:balance).of_type(:decimal).with_options(null: false, precision: 3, scale: 2) }
+  it { should have_db_column(:name).of_type(:string).with_options(null: false) }
 
-  # Timestamps
-  it { should have_db_column(:active_at)  .of_type(:datetime) }
-  it { should have_db_column(:created_at) .of_type(:datetime) }
-  it { should have_db_column(:deleted_at) .of_type(:datetime) }
-  it { should have_db_column(:updated_at) .of_type(:datetime) }
+  it { should have_db_column(:created_at).of_type(:datetime) }
+  it { should have_db_column(:updated_at).of_type(:datetime) }
+
+  context 'validations' do
+    it { should validate_presence_of(:account_number) }
+    it { should validate_presence_of(:name) }
+    it { should validate_numericality_of(:balance).presence }
+  end
 
 end
