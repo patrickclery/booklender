c679a1473a139b237295ed541646b695f3211af9
patrickclery <patrick.clery@gmail.com>
2020-01-16 01:46:26 -0800

    :pencil2: Fixing typos.

--- spec/factories/transaction_factory.rb
+++ spec/factories/transaction_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :transaction do
-    amount_cents { 0.25 }
+    amount_cents { 25 }
     book
     created_at { Faker::Time.between(from: 1.year.ago, to: 10.days.ago) }
     user
