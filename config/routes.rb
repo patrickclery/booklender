82a5f6cca5753c2f29536e967c04271f724ae192
patrickclery <patrick.clery@gmail.com>
2020-01-20 23:45:47 -0800

    new :sparkles: Introducing new features. now able to get loaned books of user

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users do
     resources :transactions
+    get :loans, to: "users#loaned_books", on: :member
   end
   resources :books do
     resources :transactions
